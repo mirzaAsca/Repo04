a70ee85027061c6bee167e4a7c89c096d373d0e2
mirzaAsca <asca_mirza7@hotmail.com>
2021-09-16 23:30:38 +0200

    init done

---
+++ ex09/src/index.js
@@ -0,0 +1,23 @@
+function main(){
+
+    // Only change code below this line
+
+    var firstNum = 6;
+    var secondNUM = 25;
+    var thirdNum = 5.2;
+    firstNum *= 9;
+    secondNUM /= 5;
+    thirdNum *= 10;
+    
+    // Only change code above this line
+
+     return {
+         firstNum,
+         secondNUM,
+         thirdNum
+        };
+
+}
+
+console.log(main());    
+module.exports = main;
---
+++ ex10/src/index.js
@@ -0,0 +1,21 @@
+function main(){
+
+    // Only change code below this line
+
+    var myFirstName = "Mirza";
+    var myLastName = "Asceric";
+    var myHomeTown = "Sarajevo";
+    
+    
+    // Only change code above this line
+
+     return {
+         myFirstName,
+         myLastName,
+         myHomeTown
+        };
+
+}
+
+console.log(main());    
+module.exports = main;
