803c4bb48aab644ca72e1e35d450d7f1ac6ef96c
mirzaAsca <asca_mirza7@hotmail.com>
2021-09-17 10:08:40 +0200

    prepravke ex09, ex12, ex13, ex14, ex15

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -3,17 +3,17 @@ function main(){
     // Only change code below this line
 
     var firstNum = 6;
-    var secondNUM = 25;
+    var secondNum = 25;
     var thirdNum = 5.2;
     firstNum *= 9;
-    secondNUM /= 5;
+    secondNum /= 5;
     thirdNum *= 10;
     
     // Only change code above this line
 
      return {
          firstNum,
-         secondNUM,
+         secondNum,
          thirdNum
         };
 
